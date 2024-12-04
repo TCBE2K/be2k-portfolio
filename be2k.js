52ac482af0cc46e99c4527afff942d470a7daac8
Ethan Smith <me@be2k.net>
2024-12-04 07:38:47 -0600

    v1

--- be2k.js
+++ be2k.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
+import terminalImage from 'terminal-image';
+import chalk from 'chalk';
 
-import { input } from '@inquirer/prompts';
+console.log(await terminalImage.file('me.png'));
 
-const answer = await input({ message: 'Enter your name' });
-console.log('Your name is: ' + answer);
+console.log(chalk.bgGreen('I am ' + chalk.bold(chalk.cyan(('BE2K\n')))))
+
+console.log(chalk.blue("I am a developer based in Kansas City!\nI play all sorts of games, like Destiny, Minecraft, and more!\n\nYou can find me at these links:\n\n"));
+console.log(chalk.red("Email: me@be2k.net\nGithub: https://github.com/be2k\nMore coming soon..."))
